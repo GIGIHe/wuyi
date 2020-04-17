180165d75d44341f403b65d74f53a84a5fe724e8
GIGIHe <2697190181@qq.com>
2020-04-17 17:04:36 +0800

    4545

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+$(function() {
+    $(".x_close").click(function(){
+        $('.mask,.mask_t').fadeOut()
+    })
+    setTimeout(function(){
+        $('.mask,.mask_t').fadeOut()
+    },4000)
+})
